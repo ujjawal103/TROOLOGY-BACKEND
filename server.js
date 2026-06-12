f6d298d87ca39114849bf6088dceb6a5ad643037
ujjawal103 <ujjawaljaiswal618@gmail.com>
2026-06-13 01:32:25 +0530

    i thought to add socke.io but for now keep it simple

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const http = require("http")
 const app = require("./app")
-const { initializeSocket } = require("./socket");
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
-
-
- initializeSocket(server);
  
 server.listen(port , ()=>{
     console.log("server is running on port :" , port);
-   
 })
